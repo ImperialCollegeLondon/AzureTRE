2bfd477e445f933db2c051796b6040e070ad9123
tim-alen-ck <tim.allen@cloudkubed.com>
2023-10-13 15:02:25 +0100

    update nav img

--- ui/app/src/components/shared/TopNav.tsx
+++ ui/app/src/components/shared/TopNav.tsx
@@ -12,7 +12,7 @@ export const TopNav: React.FunctionComponent = () => {
         <Stack horizontal>
           <Stack.Item grow={100}>
             <Link to='/' className='tre-home-link'>
-              <img src={logo} alt="IC Logo" height="50px" style={{ marginLeft: '10px', marginRight: '10px', verticalAlign: 'middle' }} />
+              <img src={logo} alt="Imperial College Logo" style={{ marginLeft: '10px', marginRight: '10px', verticalAlign: 'middle' }} />
               <h5 style={{display: 'inline'}}>Imperial TRE</h5>
             </Link>
           </Stack.Item>
