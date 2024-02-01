405ac035647ddbc3886682459215e0631f7b1db1
tim-alen-ck <tim.allen@cloudkubed.com>
2024-02-01 12:16:35 +0000

    update api

--- api_app/services/aad_authentication.py
+++ api_app/services/aad_authentication.py
@@ -35,7 +35,7 @@ class AzureADAuthorization(AccessService):
     aad_instance = config.AAD_AUTHORITY_URL
 
     TRE_CORE_ROLES = ['TREAdmin', 'TREUser']
-    WORKSPACE_ROLES_DICT = {'WorkspaceOwner': 'app_role_id_workspace_owner', 'ImperialWorkspaceResearcher': 'app_role_id_Imperial_workspace_researcher', 'WorkspaceResearcher': 'app_role_id_workspace_researcher', 'AirlockManager': 'app_role_id_workspace_airlock_manager'}
+    WORKSPACE_ROLES_DICT = {'WorkspaceOwner': 'app_role_id_workspace_owner', 'ImperialWorkspaceResearcher': 'app_role_id_imperial_workspace_researcher', 'WorkspaceResearcher': 'app_role_id_workspace_researcher', 'AirlockManager': 'app_role_id_workspace_airlock_manager'}
 
     def __init__(self, auto_error: bool = True, require_one_of_roles: Optional[list] = None):
         super(AzureADAuthorization, self).__init__(
