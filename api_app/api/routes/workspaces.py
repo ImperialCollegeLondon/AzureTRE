d7b6f1b54aad06e2e84521749d339a27ad4b0cc6
tim-alen-ck <tim.allen@cloudkubed.com>
2024-02-02 16:32:32 +0000

    change back ImperialWorkspaceResearcher role

--- api_app/api/routes/workspaces.py
+++ api_app/api/routes/workspaces.py
@@ -45,7 +45,7 @@ user_resources_workspace_router = APIRouter(dependencies=[Depends(get_current_wo
 
 
 def validate_user_has_valid_role_for_user_resource(user, user_resource):
-    if "WorkspaceOwner" in user.roles or "ImperialWorkspaceResearcher" in user.roles:
+    if "WorkspaceOwner" in user.roles:
         return
 
     if ("WorkspaceResearcher" in user.roles or "AirlockManager" in user.roles) and user_resource.ownerId == user.id:
@@ -54,8 +54,8 @@ def validate_user_has_valid_role_for_user_resource(user, user_resource):
     # if ("WorkspaceResearcher" in user.roles or "AirlockManager" in user.roles or "ImperialWorkspaceResearcher" in user.roles) and user_resource.ownerId == user.id:
     #     return
     
-    # if "ImperialWorkspaceResearcher" in user.roles and user_resource.ownerId == user.id:
-    #     return
+    if "ImperialWorkspaceResearcher" in user.roles and user_resource.ownerId == user.id:
+        return
 
     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=strings.ACCESS_USER_IS_NOT_OWNER_OR_RESEARCHER)
 
