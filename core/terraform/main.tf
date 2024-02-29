76348e201f3affe0062c6009709665718ff24d03
tim-alen-ck <tim.allen@cloudkubed.com>
2024-02-29 16:10:09 +0000

    update

--- core/terraform/main.tf
+++ core/terraform/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.74.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.20"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.4.0"
